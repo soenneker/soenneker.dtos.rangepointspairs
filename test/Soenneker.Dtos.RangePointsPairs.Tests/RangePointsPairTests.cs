6f4f5e05788aebcadb743fd853dc3d414157cff6
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:41 -0500

    TUnit conversion

--- test/Soenneker.Dtos.RangePointsPairs.Tests/RangePointsPairTests.cs
+++ test/Soenneker.Dtos.RangePointsPairs.Tests/RangePointsPairTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Dtos.RangePointsPairs.Tests;
 
 public sealed class RangePointsPairTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
